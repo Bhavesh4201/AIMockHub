74360629bbf7cc44abc5f41065b9654b2d0cfd0c
Bhavesh Sirsath <121222683+bhaveshkoli@users.noreply.github.com>
2025-11-18 14:58:38 +0530

    Implement user login functionality: add validation, authentication, and JWT token generation; update user routers to include login route.

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -36,3 +36,41 @@ export const userRegister = async (req, res) => {
     console.log(error);
   }
 };
+
+
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id },
+      config.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).json({
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
